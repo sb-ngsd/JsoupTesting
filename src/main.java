0ea563cd4acae85e9d32b7248ba3c1250918f448
PaperTools <unconfigured@null.destroystokyo.com>
2018-01-16 20:01:54 -0600

    android port prep

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,25 +14,29 @@ import org.jsoup.select.Elements;
 public class main {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
-		String url = "http://openings.ninja/Kill+la+Kill/op/1";
-		String oped = "ed";
+		
+		String animeSearch = "Kill+la+kill";
+		String url = "http://openings.ninja/" + animeSearch + "/op/1";
 		Document doc = Jsoup.connect(url).get();
         
 		ArrayList<String> ar = new ArrayList<String>();
-		
+		String themeout = null;
 		for (Element link : doc.select("#mirror > li")) {
 		    String urlout = link.attr("data-url");
-		    String themeout = link.attr("data-theme");
+		    themeout = link.attr("data-theme");
 		    //String output = urlout;
 		    String output = themeout+":" + urlout;
 		    if(output.indexOf("youtube") <0 && output.indexOf("web.archive.org") <0){
-		    	//output = output.substring(5);
+		    	//String outputlink = output.substring(themeout.length());
 		    	ar.add(output);
 	        }
-		}   
-		System.out.println(ar);
+		}
+		
+		//System.out.println(ar);
+		int mirrorSelect = 0;
+		
 		if (Desktop.isDesktopSupported()) {
-		    Desktop.getDesktop().browse(new URI(ar.get(0)));
+		    Desktop.getDesktop().browse(new URI(ar.get(mirrorSelect).substring(themeout.length() + 1)));
 		}
 		
 		
